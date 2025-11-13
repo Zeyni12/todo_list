6942a800aa41687a7d59be4eb874d7f48b6b15d7
zeyni12 <zeynebmu40@gmail.com>
2025-11-13 16:09:50 +0300

    filter

--- base/views.py
+++ base/views.py
@@ -68,6 +68,17 @@ class RegisterPage(FormView):
     form_class = UserCreationForm
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)  
 
 def logout_user(request):
     """Logs the user out and redirects to the login page."""
@@ -82,6 +93,11 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+        context['search_input']= search_input    
         return context   
 
 class TaskDetail(LoginRequiredMixin, DetailView):

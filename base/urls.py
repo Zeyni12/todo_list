c96cbad6e241e17f5f15f59f98914dc817f82282
zeyni12 <zeynebmu40@gmail.com>
2025-11-13 11:13:11 +0300

    logout

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,28 @@
+# from django.urls import path
+# from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, CustomLogoutView
+
+# urlpatterns = [
+#     path('login/', CustomLoginView.as_view(), name='login'),
+#     path('logout/', CustomLogoutView.as_view(next_page='login'), name='logout'),
+#     path('', TaskList.as_view(), name='tasks'),
+#     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+#     path('task-create/', TaskCreate.as_view(), name='task-create'),
+#     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+#     path('task-delete/<int:pk>/',  DeleteView.as_view(), name='task-delete'),
+# ]
+
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
-from django.contrib.auth.views import LogoutView
+from .views import (
+    TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView,
+    CustomLoginView, logout_user
+)
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', logout_user, name='logout'), 
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
-    path('task-delete/<int:pk>/',  DeleteView.as_view(), name='task-delete'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,40 +1,95 @@
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+# from django.views.generic.list import ListView
+# from django.views.generic.detail import DetailView
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.urls import reverse_lazy
+# from django.contrib.auth.views import LogoutView
 
-from django.contrib.auth.views import LoginView
+# from django.contrib.auth.views import LoginView
+
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# from .models import Task
+# # Create your views here.
+
+# class CustomLoginView(LoginView):
+#     template_name = 'base/login.html'
+#     fields = '__all__'
+#     redirect_authenticated_user = True
+    
+#     def get_success_url(self):
+#         return reverse_lazy('tasks')
+    
+# class CustomLogoutView(LogoutView):
+#     # This allows GET requests (e.g., when clicking a logout link)
+#     def get(self, request, *args, **kwargs):
+#         return self.post(request, *args, **kwargs)
+# class TaskList(LoginRequiredMixin, ListView):
+#     model = Task
+#     context_object_name = 'tasks'
+    
+# class TaskDetail(LoginRequiredMixin, DetailView):
+#     model = Task
+#     context_object_name = 'task'
+#     template_name = 'base/task.html'    
+    
+# class TaskCreate(LoginRequiredMixin, CreateView):    
+#     model = Task
+#     fields = '__all__'
+#     success_url = reverse_lazy('tasks')
+    
+# class TaskUpdate(LoginRequiredMixin, UpdateView): 
+#     model = Task  
+#     fields = '__all__'
+#     success_url = reverse_lazy('tasks')
+    
+# class DeleteView(LoginRequiredMixin, DeleteView):
+#     model = Task
+#     context_object_name = 'task'
+#     success_url = reverse_lazy('tasks')   
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
-# Create your views here.
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
-    fields = '__all__'
     redirect_authenticated_user = True
-    
+
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(ListView):
+# class CustomLogoutView(LogoutView):
+#     def get(self, request, *args, **kwargs):
+#         return self.post(request, *args, **kwargs)
+
+def logout_user(request):
+    """Logs the user out and redirects to the login page."""
+    logout(request)
+    return redirect('login')
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
-    
-class TaskDetail(DetailView):
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
-    template_name = 'base/task.html'    
-    
-class TaskCreate(CreateView):    
+    template_name = 'base/task.html'
+
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
-    
-class TaskUpdate(UpdateView): 
-    model = Task  
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
-    
-class DeleteView(DeleteView):
+
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
-    success_url = reverse_lazy('tasks')   
+    success_url = reverse_lazy('tasks')
